7c4fba32ceb1fa0925aed662ae37a2722b00141b
NurbolatSatybaldy <n.satybaldi2003@gmail.com>
2025-02-22 01:23:15 +0500

    DONE

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://nurbolat:968510@localhost/flowers"
+SQLALCHEMY_DATABASE_URL = os.environ.get("SQLALCHEMY_DATABASE_URL")
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
