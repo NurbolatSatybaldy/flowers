4e0f2acf3f51c1b75cbee3801b1d884bc099edeb
NurbolatSatybaldy <n.satybaldi2003@gmail.com>
2025-02-22 20:50:06 +0500

    DONE

--- database.py
+++ database.py
@@ -2,22 +2,19 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+# Use SQLite as the default database if no environment variable is set.
+SQLALCHEMY_DATABASE_URL = os.environ.get("SQLALCHEMY_DATABASE_URL", "sqlite:///./flowers.db")
 
-SQLALCHEMY_DATABASE_URL = os.environ.get(
-    "SQLALCHEMY_DATABASE_URL",
-    "sqlite:///./flowers.db"
-)
-
+# For SQLite, we need this extra option.
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False} if "sqlite" in SQLALCHEMY_DATABASE_URL else {}
+    connect_args={"check_same_thread": False} if SQLALCHEMY_DATABASE_URL.startswith("sqlite") else {}
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 def get_db():
-    """Yields a new database session for each request."""
     db = SessionLocal()
     try:
         yield db
@@ -25,6 +22,5 @@ def get_db():
         db.close()
 
 def init_tables():
-    """Create tables if they do not already exist."""
     from models import Base
     Base.metadata.create_all(bind=engine)
