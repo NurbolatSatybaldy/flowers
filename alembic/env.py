c60c3d9ab3e834c6dc2db6eceb82303cf43e2ca0
NurbolatSatybaldy <n.satybaldi2003@gmail.com>
2025-02-22 19:56:58 +0500

    D

--- alembic/env.py
+++ alembic/env.py
@@ -2,10 +2,9 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
 from alembic import context
+from models import *
 from database import Base
-from database import *
 
 
 # this is the Alembic Config object, which provides
